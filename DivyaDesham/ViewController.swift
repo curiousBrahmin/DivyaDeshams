ac76dedf6ca58ddb427bd0a67f72fee058ae33dc
Rambo <Rambo@Hasthakala.local>
2016-10-07 00:26:42 +0530

    Initial Commit

---
+++ DivyaDesham/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DivyaDesham
+//
+//  Created by Rambo on 10/7/16.
+//  Copyright © 2016 CuriousBrahmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
